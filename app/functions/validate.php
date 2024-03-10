d00fb3576de633bba8e6736c66710122cdd5a549
Emerson <emerson.leite@evacard.com.br>
2024-03-09 21:10:44 -0300

    chore: update validate.php

--- app/functions/validate.php
+++ app/functions/validate.php
@@ -1,8 +1,18 @@
 <?php
 
-function validate(array $fields): bool
+function validate(array $fields): object
 {
 
+    $sanitize = ["email" => FILTER_SANITIZE_EMAIL,
+        "name" => FILTER_SANITIZE_STRING,
+        "subject" => FILTER_SANITIZE_STRING,
+        "message" => FILTER_SANITIZE_STRING];
+    
+    $validate = [];
 
-    return true;
+    foreach ($fields as $key => $value) {
+        $validate[$key] = filter_var($value, $sanitize[$key]);
+
+    }
+    return (object)$validate;
 }
