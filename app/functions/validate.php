77640d366a3289a1bda5d386f1254aff93cf49c9
Emerson <emerson.leite@evacard.com.br>
2024-03-11 20:57:04 -0300

    chore: update validate.php from app/functions

--- app/functions/validate.php
+++ app/functions/validate.php
@@ -1,7 +1,7 @@
 <?php
 
 
-function validate(array $fields): object
+function validateFields(array $fields): object
 {
 
     $validateEmail = function (string $string): string {
@@ -30,4 +30,15 @@ function validate(array $fields): object
 
     }
     return (object)$validate;
+}
+
+function isEmpty(array $fields): bool
+{
+    $empty = false;
+    foreach ($fields as $key => $value) {
+        if (empty($value)) {
+            $empty = true;
+        }
+    }
+    return $empty;
 }
