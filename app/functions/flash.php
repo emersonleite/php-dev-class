bafdd13a8ffc31cde249ae59713b71024cb57356
Emerson <emerson.leite@evacard.com.br>
2024-03-11 20:56:05 -0300

    chore: update flash.php

---
+++ app/functions/flash.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function flash($key, $message, $type = "danger"): void
+{
+    if (!isset($_SESSION['flash'][$key])) {
+        $_SESSION['flash'][$key] = "<div class='alert alert-{$type}'>{$message}</div>";
+    }
+}
+
+function get($key)
+{
+    if (isset($_SESSION['flash'][$key])) {
+        $message = $_SESSION['flash'][$key];
+
+        /*unset($_SESSION['flash'][$key]);*/
+
+        return $message;
+    }
+}
